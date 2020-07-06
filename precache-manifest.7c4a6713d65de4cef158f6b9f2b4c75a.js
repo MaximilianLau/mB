4cc98546b9570c2db10d46d0f774bcb354776d63
MaximilianLau <43243180+MaximilianLau@users.noreply.github.com>
2020-07-06 16:12:58 -0400

    Updates

---
+++ precache-manifest.7c4a6713d65de4cef158f6b9f2b4c75a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61e9b3c927680eed2280195df20845a1",
+    "url": "/mB/index.html"
+  },
+  {
+    "revision": "6a986025d00aa67c29ef",
+    "url": "/mB/static/css/2.6595b286.chunk.css"
+  },
+  {
+    "revision": "d5bcf4b425279e6687e1",
+    "url": "/mB/static/css/main.b4ed534d.chunk.css"
+  },
+  {
+    "revision": "6a986025d00aa67c29ef",
+    "url": "/mB/static/js/2.e18aefae.chunk.js"
+  },
+  {
+    "revision": "e805e301b4e4dff5162bf1a20225b92e",
+    "url": "/mB/static/js/2.e18aefae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5bcf4b425279e6687e1",
+    "url": "/mB/static/js/main.7bf8c44b.chunk.js"
+  },
+  {
+    "revision": "6becd881db2ac62b1545",
+    "url": "/mB/static/js/runtime-main.4395a737.js"
+  }
+]);
